8d35859b29a686c84979aac6403a51c8ea191417
bhavisshyya <bhavisshyya@gmail.com>
2023-07-11 16:12:53 +0530

    commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./navbar.scss";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <div className="navbar">
       <div className="left">
@@ -13,7 +15,7 @@ export const Navbar = () => {
         <div className="nav-opt">About us</div>
         <div className="nav-opt">FAQ</div>
         <div className="nav-opt">Sign up</div>
-        <button className="nav-btn">Login</button>
+        <button className="nav-btn" onClick={()=>navigate('/login')}>Login</button>
       </div>
     </div>
   );
